e856da46254eccd9936584833198a06f441a3d38
Collins Wamucii <collinswamucii@gmail.com>
2023-02-20 08:31:22 -0800

    Uses the printf function to output string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * printf code
+ * by: Collins Wamucii
+ */
+
+#include <stdio.h>
+
+/**
+ * main - uses the printf function to output
+ * followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+
+	return (0);
+}
