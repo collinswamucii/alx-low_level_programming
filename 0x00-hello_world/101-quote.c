28bf2ef9b799905c8bbab6843c9e1faa38833f26
Collins Wamucii <collinswamucii@gmail.com>
2023-02-20 10:30:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,10 @@
 
 int main(void)
 {
-	char *p = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, p, strlen(p));
+	write(2,
+		"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",
+		59);
 	
 	return (1);
 }
