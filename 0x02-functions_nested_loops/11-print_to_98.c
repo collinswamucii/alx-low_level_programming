7ca5a31395c090926ec7448a3051e7155394ab52
Collins Wamucii <collinswamucii@gmail.com>
2023-02-22 12:50:52 -0800

    I used the standard library

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to-98 - prints from n to 98 separated by a comma and space
+ * @n: the starting poinr of the integer
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+}
