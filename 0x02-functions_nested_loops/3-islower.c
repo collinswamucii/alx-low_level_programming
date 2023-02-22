c4884ed7e2915b00842ecdddc2fa028f4790b151
Collins Wamucii <collinswamucii@gmail.com>
2023-02-22 10:29:59 -0800

    checks for character c if it is lowercase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - checks for c lowercase or not
- * c: character to be checked
+ * @c: character to be checked
  * Return: 1 if character is lowercase, 0 if otherwise
  */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
