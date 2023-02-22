6e44d2f4710abd57655d63baae4c5ed6ab7626ef
Collins Wamucii <collinswamucii@gmail.com>
2023-02-22 08:00:21 -0800

    Using the _putchar function to print "_putchar"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - uses function _putchar to print "_putchar"
+ * then follows a new line
+ * Return: (0)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
