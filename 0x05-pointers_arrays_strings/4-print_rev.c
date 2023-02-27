5ad61785b1f1ae6c30eedb7e9af2d2743cf839a5
Collins Wamucii <collinswamucii@gmail.com>
2023-02-27 15:49:52 -0800

    Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,13 @@
  * Return: Nothing
 */
 
-void print_rev(char *s)
-{
-	int index;
-
-	/*finds the length of string without null character*/
-	for (index = 0; s[index] != '\0'; ++index)
-		;
-
-	/*print char from the last index as you decrement*/
-	for (--index; index >= 0; --index)
-		_putchar(s[index]);
-	_putchar('\n');
+void print_rev(char *s) {
+    int len = strlen(s);
+    char reversed[len+1];
+    for (int i = 0; i < len; i++) {
+        reversed[i] = s[len-1-i];
+    }
+    reversed[len] = '\0';
+    printf("%s\n", reversed);
 }
+
